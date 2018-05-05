4e832777d97ff55668f0713b7b1114e7d21a2d99
桐铭 梁 <werltm@hotmail.com>
2018-05-05 15:58:58 +0800

    修改代码处理点bug

--- src/angular/SPACore.PhoneBook/angular-phonebook/src/shared/service-proxies/service-proxy.module.ts
+++ src/angular/SPACore.PhoneBook/angular-phonebook/src/shared/service-proxies/service-proxy.module.ts
@@ -14,7 +14,7 @@ import * as ApiServiceProxies from './service-proxies';
         ApiServiceProxies.AccountServiceProxy,
         ApiServiceProxies.ConfigurationServiceProxy,
         ApiServiceProxies.PersonServiceProxy,
-        ApiServiceProxies.EnumServiceProxy
+        ApiServiceProxies.EnumServiceProxy,
         { provide: HTTP_INTERCEPTORS, useClass: AbpHttpInterceptor, multi: true }
     ]
 })
